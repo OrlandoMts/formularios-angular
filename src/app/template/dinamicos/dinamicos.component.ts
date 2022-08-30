f44350273d97474996bd4696cc2058e4ef6c6d02
Orlando Montes <omontes@indelpro.com>
2022-08-30 15:23:38 -0500

    termine el componente template/dinamicos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[]
+};
+
+interface Favorito {
+  id: number;
+  nombre: string
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -8,9 +19,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
+  @ViewChild('formDinamico') formDinamico!: NgForm;
+  juegoNuevo: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  persona: Persona = {
+    nombre: 'orlando',
+    favoritos: [
+      {id: 11, nombre:'animal crossing'},
+      {id:2, nombre:'forza'}
+    ]
+  }
+
+  guardar(){
+    console.log('xd');
+    this.formDinamico.resetForm();
+  }
+
+  eliminar(index: number) {
+    this.persona.favoritos.splice(index,1)
+  }
+
+  guardarFavorito() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.juegoNuevo
+    }
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.juegoNuevo = "";
   }
 
 }
